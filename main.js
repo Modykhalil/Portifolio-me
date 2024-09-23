313a462ae386b1c7b21970363dc457340b20deef
modykhalil <modekhalil86@gmail.com>
2024-09-23 20:39:03 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+// up to top
+let up = document.querySelector(".up");
+window.onscroll = () => {
+  console.log(window.scrollY);
+  if (window.scrollY >= 400) {
+    up.style.transform = "scale(1)";
+  } else {
+    up.style.transform = "scale(0)";
+  }
+};
+
+up.addEventListener("click", () => {
+  window.scrollTo(0, 0);
+});
+
+// header toggle menu
+let toggle = document.querySelector(".header .container i");
+let links = document.querySelector(".header .container .links");
+
+toggle.addEventListener("click", () => {
+  links.classList.toggle("active");
+});
+
+// landing
+let me = ["Frontend Developer",];
+let txt = document.querySelector(".landing .container .text h2 span ");
+let index = 0;
+
+function rand() {
+  txt.innerHTML = me[index];
+  index++;
+  if (index == me.length) {
+    index = 0;
+  }
+  setTimeout(rand, 2000);
+}
+rand();
+
+// donwload cv
+let btn = document.querySelector(".landing .container .text .btn");
+let link = document.querySelector(".landing .container .text .btn a");
+btn.addEventListener("click", () => {
+  link.setAttribute("download", "cv_Mohamed Khalil.pdf");
+  link.click();
+});
